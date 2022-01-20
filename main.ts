fc752856bd681c578ec8f6b31f834f4729d40f86
casrap <90317644+casrap@users.noreply.github.com>
2022-01-20 13:37:01 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,84 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . . . .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . #
+        . . . . #
+        . # . . #
+        . # . . .
+        . # . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . . # .
+        . . . # .
+        # . . # .
+        # . . . .
+        # . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        . # . . #
+        . # . . #
+        . . . . #
+        . # . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . . . .
+        # . . # .
+        # . . # .
+        . . . # .
+        # . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(400)
+    basic.showLeds(`
+        . . . . #
+        . # . . #
+        . # . . #
+        . # . . .
+        . # . . #
+        `)
+})

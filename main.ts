729b54eedd355c04b2c08c122ce702fe235ebdd1
casrap <90317644+casrap@users.noreply.github.com>
2022-01-20 13:42:43 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -129,4 +129,44 @@ basic.forever(function () {
         . # . . .
         . # . . #
         `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . . # .
+        . . . # .
+        # . . # .
+        # . . . .
+        # . . # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . #
+        . . # . #
+        . . # . .
+        . . . . #
+        . . # . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . . .
+        . . . # .
+        . # . # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . . . #
+        . . # . .
+        # . # . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . # .
+        . # . . .
+        . # . # .
+        `)
 })
